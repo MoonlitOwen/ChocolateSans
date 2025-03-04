eb84ef390dd06529af66f7bba58b1bf6e756ba1a
Aaron <aaron@sajatypeworks.com>
2025-03-03 16:32:46 -0800

    GF rebuild
    - added build system following ThenKhung model
    - Added BASE table
    - Updated license string
    - Added AUTHORs file
    - unmap softhyphen
    - vertical metrics
    - version string metadata cleared in
    - update postscript name
    - renamed some very long glyph names:
    * katakanasmallhu_katakanahiraganasemivoicedsoundmarkcombkana.ccmp
    * katakanasmallhu_katakanahiraganasemivoicedsoundmarkcombkana.ccmp.1
    
    - Adjust anchor in glyphs and add anchors to dotted circle ($25CC)
    * uni0300
    * uni0301
    * uni0307
    * uni030C
    
    - uni9E44 - fixed overlap issue due to overlapping contours where is quadratic and one is cubic.
    
    - Cleared overlapping segments / duplicate contours on various glyphs
    * uni50C6
    * uni5966
    * uni79BB
    * uni7E34
    * uni8506
    * uni8867
    * uni9C40

---
+++ source/fcp_ufo_process.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python3
+
+import glob
+from pathlib import Path
+import re
+import shutil
+import codecs
+import ufoLib2
+from fontTools.pens.reverseContourPen import ReverseContourPen
+from fontTools.pens.areaPen import AreaPen
+import copy
+
+SOURCE = Path("source/temp")
+
+# Processing invalid UFO generation from Font Creator version 15.0.0.2974
+
+for UFO in SOURCE.glob("*.ufo"):
+
+	# Clearing BOM encoding in older versions of FCP. 
+    for file in UFO.rglob("*.*"):
+        try:
+            with open(str(file), 'rb') as f:
+                content = f.read()
+            
+            if content.startswith(codecs.BOM_UTF8):
+                content = content[len(codecs.BOM_UTF8):]
+                with open(file, 'wb') as f:
+                    f.write(content)
+            else:
+                pass
+        except Exception as e:
+            print(f"An error occurred: {e}")
+
+
+
+	# Older FCP does not add the encodingID properly
+    fontInfo = UFO / "fontinfo.plist"
+
+    plist = open(fontInfo, 'r')
+    content = plist.read()
+    content_new = re.sub(
+		'<key>platformID</key>\\n\\s+<integer>3</integer>\\n\\s+<key>encodingID</key>\\n\\s+<integer>.*</integer>', 
+		'<key>platformID</key><integer>3</integer><key>encodingID</key><integer>1</integer>', 
+		content
+	)
+    plist.close()
+
+    with open (fontInfo, 'w') as plistWriter:
+        plistWriter.write (content_new)
+
+	# Correcting invalid feature file data in older FCP output
+    features = UFO / "features.fea"
+
+    fea = open(features, 'r')
+    content = fea.read()
+
+    content_new = re.sub(
+		r"@\\", 
+		"@", 
+		content
+	)
+    fea.close()
+
+    with open (features, 'w') as feaWriter:
+        feaWriter.write(content_new)
+
+# ------------------------------------
+
+    # Now that the UFO is normalized, we can modify it more easily with ufoLib2
+    print ("Adding some name records")
+    font = ufoLib2.Font.open(UFO)
+
+    # Setting useTypoMetrics flag
+    font.info.openTypeOS2Selection.append(7)
+
+    name_records = []
+
+    for record in font.info.openTypeNameRecords:
+        #FCP creates a NID 16 for the localized name, but not a NID1
+        if record.get("nameID") == 16:
+            new_record = {
+                "nameID": 1,
+                "platformID": record.get("platformID"),
+                "encodingID": record.get("encodingID"),
+                "languageID": record.get("languageID"),
+                "string": record.get("string"),
+            }
+            name_records.append(new_record)
+        elif record.get("nameID") == 17:
+            pass
+        else:
+            name_records.append(record) 
+    font.info.openTypeNameRecords = name_records
+
+# ------------------------------------
+
+    # Add the BASE table
+
+    base_table_content = """
+table BASE {
+HorizAxis.BaseTagList   icfb    icft    ideo    romn;
+HorizAxis.BaseScriptList  
+    DFLT    ideo  	-77   793  -142     0,
+    hani    ideo   	-77   793  -142     0,
+    kana    ideo   	-77   793  -142     0,
+    hang    ideo   	-77   793  -142     0,
+    latn    romn   	-77   793  -142     0;
+
+VertAxis.BaseTagList    icfb    icft    ideo    romn;
+VertAxis.BaseScriptList
+    DFLT    ideo    65   935     0   142,
+    hani    ideo    65   935     0   142,
+    kana    ideo    65   935     0   142,
+    hang    ideo    65   935     0   142,
+    latn    romn    65   935     0   142;
+} BASE;
+"""
+
+    features = font.features.text if font.features.text else ""
+    if "table BASE" in features:
+        pass
+    else:
+        print("Adding BASE table to features.fea")
+        features += f"\n\n{base_table_content}\n"
+        font.features.text = features
+
+    font.save(UFO,overwrite=True)
